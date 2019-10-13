b3816c5a271202713ddafdd41e03a45dcfa215a0
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-10-13 10:33:33 +0200

    initial commit

---
+++ src/main/java/guru/springframework/webfluxstackquoteservice/WebfluxStackQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.webfluxstackquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStackQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxStackQuoteServiceApplication.class, args);
+    }
+
+}
